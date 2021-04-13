a27609dba215fb9058dbc5fe72fc8eaa4d8d7983
Angus Leung <prototype032@gmail.com>
2021-04-13 14:51:43 +1000

    added script for adding toolboxes

---
+++ add_toolbox.m
@@ -0,0 +1,13 @@
+%% Description
+
+%{
+
+Run this before running anything else in MASSIVE
+
+Add hctsa toolbox (and anything else, for MASSIVE)
+
+%}
+
+toolbox_dir = 'toolboxes/';
+
+addpath(genpath(toolbox_dir));

6a42094a8a1b14f2363307af354b24da09a79c13
Angus Leung <prototype032@gmail.com>
2023-01-31 12:41:46 +1100

    fixed wrong filename

--- bash_loop_sbatch.bash
+++ bash_loop_sbatch.bash
@@ -4,6 +4,6 @@ increment=1
 # Loop through channels
 for (( ch=1; ch<=$nChannels; ch=$ch+1 )); do
 	echo $ch
-	sbatch --job-name="$ch_multidose" --output="logs/multidose_${ch}.out" --error="logs/multidose_${ch}.err" bash_loop_sbatch.bash $ch
+	sbatch --job-name="$ch_multidose" --output="logs/multidose_${ch}.out" --error="logs/multidose_${ch}.err" sbatch_hctsa_2_multidose.bash $ch
 	echo "submitted channel $ch"
 done

8ee4daf1ef1b4d3f43f73ac90d19b27d9806d4f5
Angus Leung <prototype032@gmail.com>
2023-01-31 13:04:11 +1100

    updated to call slurm parallel capable(?) version of function

--- sbatch_hctsa_2_multidose.bash
+++ sbatch_hctsa_2_multidose.bash
@@ -47,7 +47,7 @@ module load matlab/r2019b
 # Create a temporary directory on scratch
 mkdir -p $SCRATCH/$SLURM_JOB_ID
 
-time matlab -nodisplay -nodesktop -r "add_toolbox; main_hctsa_2_compute('HCTSA_multidose_channel${1}.mat'); exit"
+time matlab -nodisplay -nodesktop -r "add_toolbox; main_hctsa_2_compute_slurmParallel('HCTSA_multidose_channel${1}.mat'); exit"
 
 # Cleanup local work directory
 rm -rf $SCRATCH/$SLURM_JOB_ID

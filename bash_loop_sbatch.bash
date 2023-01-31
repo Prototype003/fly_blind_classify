768e7eab23c56d1db0194d59884d1b24e6973a27
Angus Leung <prototype032@gmail.com>
2023-01-31 12:44:58 +1100

    fixed job name

--- bash_loop_sbatch.bash
+++ bash_loop_sbatch.bash
@@ -4,6 +4,6 @@ increment=1
 # Loop through channels
 for (( ch=1; ch<=$nChannels; ch=$ch+1 )); do
 	echo $ch
-	sbatch --job-name="$ch_multidose" --output="logs/multidose_${ch}.out" --error="logs/multidose_${ch}.err" sbatch_hctsa_2_multidose.bash $ch
-	echo "submitted channel $ch"
+	sbatch --job-name="${ch}_multidose" --output="logs/multidose_${ch}.out" --error="logs/multidose_${ch}.err" sbatch_hctsa_2_multidose.bash $ch
+	echo "submitted channel ${ch}"
 done
